72a1c0627aec184cd79ffc896f354ac0dd0be38e
Jonathan0 <johnlin989@gmail.com>
2022-02-23 22:42:46 -0500

    update assignment1 coding 13,14,15

--- src/main/java/day2/Assignment1.java
+++ src/main/java/day2/Assignment1.java
@@ -1,11 +1,21 @@
 package day2;
 
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Assignment1 {
 
     public static void main(String [] args) {
         testInput();
+
+        int [] a = new int[]{-6,-2,-3,-4,-5,-1};
+        int [] b = new int[]{1,2,3,4,5,6,7};
+
+        int [] newAB = merge2Arrays(a,b);
+        System.out.println(newAB.toString());
+
+        System.out.println(findSecond(a));
     }
 
     // 13. write a program for the following activity
@@ -33,5 +43,39 @@ public class Assignment1 {
             System.out.println("Displace result " + n);
             n--;
         }
+    }
+
+    // question 14 merge 2 arrays
+    public static int [] merge2Arrays(int [] a, int [] b) {
+        int lenA = a.length;
+        int lenB = b.length;
+        int [] newAB = new int[lenA+lenB];
+
+        int i = 0;
+        for(int valueA : a) {
+            newAB[i] = valueA;
+            i++;
+        }
+        for(int valueB : b) {
+            newAB[i] = valueB;
+            i++;
+        }
+        return newAB;
+    }
+
+    // question 15 find the second large number in array
+    public static int findSecond(int [] a) {
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+
+        for(int valueA : a) {
+            set.add(valueA);
+        }
+
+        if(set.size() < 2) {
+            return -1;
+        }
+        set.pollFirst();
+
+        return set.first();
     }
 }
